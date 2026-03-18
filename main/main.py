8da3d651d793a91d211448fa68d21e074f18ee13
laozi4vip <124229392+laozi4vip@users.noreply.github.com>
2026-03-19 03:32:17 +0800

    Update main.py

--- main/main.py
+++ main/main.py
@@ -8,6 +8,7 @@ GitHub：https://github.com/laozi4vip/QuickLauncher
 __author__ = "laozi4vip"
 __github__ = "https://github.com/laozi4vip/QuickLauncher"
 __app_name__ = "QuickLauncher"
+__version__ = "2.0"
 __description__ = "Windows 任务栏快捷启动器"
 
 import wx
