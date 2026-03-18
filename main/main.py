fc7aca6e4f804c1f3d234a8968ad979964e68db2
laozi4vip <124229392+laozi4vip@users.noreply.github.com>
2026-03-19 02:56:00 +0800

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 QuickLauncher - Windows任务栏快捷启动器
-作者：xinghui
+作者：laozi4vip
 GitHub：https://github.com/laozi4vip/QuickLauncher
 功能：绑定任务栏程序到快捷键，按快捷键启动或切换窗口
 版本：1.0.36
 """
 
-__author__ = "xinghui"
+__author__ = "laozi4vip"
 __github__ = "https://github.com/laozi4vip/QuickLauncher"
 __app_name__ = "QuickLauncher"
 __description__ = "Windows 任务栏快捷启动器"
